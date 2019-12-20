1d222b8ff179e20c02c5056e3b410d9f2c26b98e
hrushikesh <hrushikesh.emmadi@gmail.com>
2019-12-20 14:11:14 +0530

    hi

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('frontend'))
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/homepage.html'))
+//res.sendFile
+//res.json
+app.get('/login', (req, res) => res.send('login'))
+
+ var student={ "name":"being zero",
+                "college":"Vnrvjiet",
+                "regno":"123445"    
+};
+app.get('/register', (req, res) => res.send('register!'))
+
+app.get('/data',(req,res)=> res.json(student))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
